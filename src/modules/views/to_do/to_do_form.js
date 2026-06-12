970cbe1699199cdd559574616cb81d6616b2008e
ZachTheMighty <promoted66@gmail.com>
2026-06-12 11:10:07 +0000

    Added: createTextInput function to to_do_form module

--- src/modules/views/to_do/to_do_form.js
+++ src/modules/views/to_do/to_do_form.js
@@ -5,11 +5,15 @@ export default function createForm(submitButtonName) {
   for (let i = 0; i < 6; i++) divs.push(document.createElement("div"));
 
   const labels = [];
+  const inputs = [];
 
   labels.push(createLabel("title"));
+  inputs.push(createTextInput("title"));
 
-  for (let i = 0; i < divs.length; i++) {
-    divs[i].push(labels[i]);
+  for (let i = 0; i < labels.length; i++) {
+    divs[i].append(labels[i]);
+    divs[i].append(inputs[i]);
+
     form.append(divs[i]);
   }
   return form;
@@ -21,4 +25,15 @@ function createLabel(name) {
   label.for = name;
 
   return label;
+}
+
+function createTextInput(name) {
+  const input = document.createElement("input");
+  input.id = name;
+  input.name = name;
+  input.autocomplete = "off";
+  input.required = name === "title" ? true : false;
+  input.autofocus = name === "title" ? true : false;
+
+  return input;
 }
