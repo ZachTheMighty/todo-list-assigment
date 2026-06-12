22d2ca9ee568a9c925f5885a0476c3cf38b527b3
ZachTheMighty <promoted66@gmail.com>
2026-06-12 09:45:57 +0000

    refactor: restructured the to do class to be a to do model the logic of created a to do object is done inside the constructor

--- src/modules/models/to_do.js
+++ src/modules/models/to_do.js
@@ -1,69 +1,16 @@
 export default class ToDo {
-  #title;
-  #description;
-  #dueDate;
-  #priority;
-  #notes;
-  #isChecked;
-
-  constructor(title, description, dueDate, priority, notes, isChecked) {
-    this.#title = title;
-    this.#description = description;
-    this.#dueDate = dueDate;
-    this.#priority = priority;
-    this.#notes = notes;
-    this.#isChecked = isChecked;
-  }
-
-  getTitle() {
-    return this.#title;
-  }
-
-  getDescription() {
-    return this.#description;
-  }
-
-  getDueDate() {
-    return this.#dueDate;
-  }
-
-  getPriority() {
-    return this.#priority;
-  }
-
-  getNotes() {
-    return this.#notes;
-  }
-
-  getIsChecked() {
-    return this.#isChecked;
-  }
-
-  setTitle(title) {
-    this.#title = title;
-  }
-
-  setDescription(description) {
-    this.#description = description;
-  }
-
-  setDueDate(dueDate) {
-    this.#dueDate = dueDate;
-  }
-
-  setPriority(priority) {
-    this.#priority = priority;
-  }
-
-  setNotes(notes) {
-    this.#notes = notes;
-  }
-
-  check() {
-    this.#isChecked = true;
-  }
-
-  unCheck() {
-    this.#isChecked = false;
+  constructor() {
+    this.todos = [
+      {
+        title: "",
+        description: "",
+        dueDate: "",
+        priority: "",
+        notes: "",
+        isChecked: false,
+        belongsTo: null,
+        id: crypto.randomUUID(),
+      },
+    ];
   }
 }
