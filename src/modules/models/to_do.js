490e4b03e94cdf5bae8d43827ce2f3439c87ce3e
ZachTheMighty <promoted66@gmail.com>
2026-06-18 16:10:15 +0000

    add: method for editing todo's contents

--- src/modules/models/to_do.js
+++ src/modules/models/to_do.js
@@ -10,4 +10,13 @@ export default class ToDoModel {
   deleteToDo(todoIndex) {
     this.todos.splice(todoIndex, 1);
   }
+
+  editTodo(todo, title, description, dueDate, priority, notes, belongsTo) {
+    todo.title = title;
+    todo.description = description;
+    todo.dueDate = dueDate;
+    todo.priority = priority;
+    todo.notes = notes;
+    todo.belongsTo = belongsTo;
+  }
 }
